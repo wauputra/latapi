259e5547c6cb1b37dce16ea51e15c7a47b3eb96e
wauputra <drioac1@gmail.com>
2019-09-05 18:07:36 +0700

    Latihan Rest Api minggu ke 2

--- wpu-hut/latihan1.php
+++ wpu-hut/latihan1.php
@@ -1,3 +1,10 @@
+<?php
+$data = file_get_contents('data/pizza.json');
+$menu = json_decode($data, true);
+
+$menu = $menu["menu"];
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -32,16 +39,21 @@
         </div>
 
         <div class="row">
-            <div class="col-md4">
-                <div class="card">
-                    <img src="img/menu/american-favourite.jpg" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">American Favourite</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <h5 class="card-title">Rp. 50.000</h5>
-                    </div>
+        <?php foreach ($menu as $row): ?>
+            <div class="col-md-4">
+            <div class="card mb-3">
+                <img src="img/menu/<?=$row["gambar"]?>" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title"><?=$row["nama"]?></h5>
+                    <p class="card-text"><?=$row["deskripsi"]?></p>
+                    <h5 class="card-title">
+                        Rp. <?=$row["harga"]?>, -
+                    </h5>
+                    <a href="#" class="btn btn-primary">Pesan sekarang</a>
+                </div>
                 </div>
             </div>
+            <?php endforeach;?>
         </div>
     </div>
 
